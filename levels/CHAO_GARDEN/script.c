2070b6902b1efa41ff120f65f56832290693fa34
Reonu <danileon95@gmail.com>
2025-01-05 18:48:15 +0000

    add chao garden music

--- levels/CHAO_GARDEN/script.c
+++ levels/CHAO_GARDEN/script.c
@@ -34,7 +34,7 @@ const LevelScript level_CHAO_GARDEN_entry[] = {
 		OBJECT(MODEL_NONE, -337, 1689, -1765, 0, 0, 0, (0x0A << 16), bhvInstantActiveWarp),
 		TERRAIN(CHAO_GARDEN_area_1_collision),
 		MACRO_OBJECTS(CHAO_GARDEN_area_1_macro_objs),
-		STOP_MUSIC(0),
+		SET_BACKGROUND_MUSIC(0x00, SEQ_CHAOGARDEN),
 		TERRAIN_TYPE(TERRAIN_GRASS),
 		/* Fast64 begin persistent block [area commands] */
 		/* Fast64 end persistent block [area commands] */
