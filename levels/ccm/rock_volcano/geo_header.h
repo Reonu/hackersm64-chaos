31a0d966643c5e290551e60cfe5032eb4b228f58
Reonu <danileon95@gmail.com>
2025-01-09 04:05:49 +0000

    level changes

---
+++ levels/ccm/rock_volcano/geo_header.h
@@ -0,0 +1,9 @@
+extern const GeoLayout rock_volcano_geo[];
+extern u8 rock_volcano_rock_ci4[];
+extern u8 rock_volcano_rock_pal_rgba16[];
+extern Vtx rock_volcano_rock_volcano_mesh_layer_1_vtx_cull[8];
+extern Vtx rock_volcano_rock_volcano_mesh_layer_1_vtx_0[36];
+extern Gfx rock_volcano_rock_volcano_mesh_layer_1_tri_0[];
+extern Gfx mat_rock_volcano_rock_volcano[];
+extern Gfx mat_revert_rock_volcano_rock_volcano[];
+extern Gfx rock_volcano_rock_volcano_mesh_layer_1[];
