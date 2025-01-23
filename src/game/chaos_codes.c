dd17147080433cb5692ce600409f22ebf2d61a67
Reonu <danileon95@gmail.com>
2025-01-23 02:21:08 +0000

    fix compiler warning

--- src/game/chaos_codes.c
+++ src/game/chaos_codes.c
@@ -124,7 +124,7 @@ void chaos_lawmetre(void) {
     if (gCrimes > 100.0f) {
         gCrimeSpawnTimer--;
         if (gCrimeSpawnTimer <= 0) {
-            struct Object *bombOmb = spawn_object_relative(0x20, 0, 300, 0, gMarioState->marioObj, MODEL_BOBOMB_COP, bhvBobomb);
+            spawn_object_relative(0x20, 0, 300, 0, gMarioState->marioObj, MODEL_BOBOMB_COP, bhvBobomb);
             gCrimeSpawnTimer = 150 / repeat;
         }
     }
