735684fed58aff31736498e63f06de1f6dd9b44c
Fazana <52551480+FazanaJ@users.noreply.github.com>
2025-01-29 15:55:23 +0000

    disabled explosion bubbles because honestly I doubt anyone notices

--- src/game/behaviors/explosion.inc.c
+++ src/game/behaviors/explosion.inc.c
@@ -13,7 +13,7 @@ void bhv_explosion_loop(void) {
     if (o->oTimer == 9) {
         if (find_water_level(o->oPosX, o->oPosZ) > o->oPosY) {
             for (i = 0; i < 40; i++) {
-                spawn_object(o, MODEL_WHITE_PARTICLE_SMALL, bhvBobombExplosionBubble);
+                //spawn_object(o, MODEL_WHITE_PARTICLE_SMALL, bhvBobombExplosionBubble);
             }
         } else {
             spawn_object(o, MODEL_SMOKE, bhvBobombBullyDeathSmoke);
