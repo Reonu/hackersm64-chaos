85be8b2dfc1ea11402b2d5323f105b201a716a7c
Fazana <52551480+FazanaJ@users.noreply.github.com>
2025-02-08 17:19:52 +0000

    fix model references

--- levels/bowser_3/script.c
+++ levels/bowser_3/script.c
@@ -43,7 +43,6 @@ const LevelScript level_bowser_3_entry[] = {
     LOAD_YAY0(/*seg*/ 0x0A, _bits_skybox_yay0SegmentRomStart, _bits_skybox_yay0SegmentRomEnd),
     ALLOC_LEVEL_POOL(),
     MARIO(/*model*/ MODEL_MARIO, /*behParam*/ 0x00000001, /*beh*/ bhvMario),
-    JUMP_LINK(script_func_global_13),
     LOAD_MODEL_FROM_GEO(MODEL_BOWSER_3_FALLING_PLATFORM_1,  bowser_3_geo_000290),
     LOAD_MODEL_FROM_GEO(MODEL_BOWSER_3_FALLING_PLATFORM_2,  bowser_3_geo_0002A8),
     LOAD_MODEL_FROM_GEO(MODEL_BOWSER_3_FALLING_PLATFORM_3,  bowser_3_geo_0002C0),
